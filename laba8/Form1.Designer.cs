63dffe2f319e83fa439cfdc8a15921e3028aec33
Home <Home@FLTP-5i5-8256>
2024-04-24 04:15:39 +0300

    Добавьте файлы проекта.

---
+++ laba8/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace laba8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ReadTheFile = new System.Windows.Forms.Button();
+            this.ApplyChanges = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.ProgressBar;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "К верхнему регистру",
+            "К нижнему регистру",
+            "1й символ к верхнему регистру",
+            "Не преобразовывать"});
+            this.comboBox1.Location = new System.Drawing.Point(625, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(267, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(622, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Укажите желаемое преобразование\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(622, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Укажите категорию изменяемых символов";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 238);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ReadTheFile
+            // 
+            this.ReadTheFile.Location = new System.Drawing.Point(39, 23);
+            this.ReadTheFile.Name = "ReadTheFile";
+            this.ReadTheFile.Size = new System.Drawing.Size(237, 99);
+            this.ReadTheFile.TabIndex = 5;
+            this.ReadTheFile.Text = "Считать файл";
+            this.ReadTheFile.UseVisualStyleBackColor = true;
+            this.ReadTheFile.Click += new System.EventHandler(this.ReadTheFile_Click);
+            // 
+            // ApplyChanges
+            // 
+            this.ApplyChanges.Location = new System.Drawing.Point(625, 315);
+            this.ApplyChanges.Name = "ApplyChanges";
+            this.ApplyChanges.Size = new System.Drawing.Size(267, 99);
+            this.ApplyChanges.TabIndex = 6;
+            this.ApplyChanges.Text = "Применить";
+            this.ApplyChanges.UseVisualStyleBackColor = true;
+            this.ApplyChanges.Click += new System.EventHandler(this.ApplyChanges_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Строки";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Столбцы";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(450, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(450, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(298, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(218, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Размерность исходной матрицы";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Русские символы",
+            "Латинские символы",
+            "Русские и латинские символы"});
+            this.comboBox2.Location = new System.Drawing.Point(625, 209);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(267, 24);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ApplyChanges);
+            this.Controls.Add(this.ReadTheFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ReadTheFile;
+        private System.Windows.Forms.Button ApplyChanges;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ laba8/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text.RegularExpressions;
+
+namespace laba8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadTheFile_Click(object sender, EventArgs e)
+        {
+            string[] fileLines = File.ReadAllLines("datas.dat.txt");
+            int rowCount = fileLines.Length;
+            int columnCount = fileLines[0].Split(' ').Length;
+
+
+
+            label5.Text = $"Размерность исходной матрицы {rowCount} ✕ {columnCount}";
+
+        }
+
+        private void ApplyChanges_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] fileLines = File.ReadAllLines("datas.dat.txt");
+                int userRowCount = Convert.ToInt32(textBox1.Text);
+                int userColumnCount = Convert.ToInt32(textBox2.Text);
+
+                string choise_1 = comboBox1.SelectedItem.ToString();
+                string choise_2 = comboBox2.SelectedItem.ToString();
+
+                string[,] subMatrix = new string[userRowCount, userColumnCount];
+                string[,] originalMatrix = new string[userRowCount, userColumnCount];
+
+
+                for (int i = 0; i < userRowCount; i++)
+                {
+                    string[] rowValues = fileLines[i].Split(' ');
+                    for (int j = 0; j < userColumnCount; j++)
+                    {
+                        subMatrix[i, j] = rowValues[j];
+                        originalMatrix[i, j] = rowValues[j];
+                    }
+
+                }
+
+                switch (choise_1)
+                {
+                    case "К верхнему регистру":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                subMatrix[i, j] = subMatrix[i, j].ToUpper(); // Преобразование к нижнему регистру
+                            }
+                        }
+                        break;
+
+                    case "К нижнему регистру":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                subMatrix[i, j] = subMatrix[i, j].ToLower(); // Преобразование к нижнему регистру
+                            }
+                        }
+                        break;
+
+                    case "1й символ к верхнему регистру":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                if (!string.IsNullOrEmpty(subMatrix[i, j]))
+                                {
+                                    subMatrix[i, j] = subMatrix[i, j][0].ToString().ToUpper() + subMatrix[i, j].Substring(1); // Преобразование 1го символа к верхнему регистру
+
+                                }
+                            }
+                        }
+                        break;
+
+                    case "Не преобразовывать":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                subMatrix[i, j] = originalMatrix[i, j];
+                            }
+                        }
+                        break;
+                }
+                switch (choise_2)
+                {
+                    case "Русские символы":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                if (Regex.IsMatch(subMatrix[i, j], @"^[\p{IsCyrillic}]+$"))
+                                {
+                                }
+                                else
+                                {
+                                    subMatrix[i, j] = originalMatrix[i, j];
+                                }
+                            }
+                        }
+                        break;
+
+                    case "Латинские символы":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                if (Regex.IsMatch(subMatrix[i, j], @"^[a-zA-z]+$"))
+                                {
+                                }
+                                else
+                                {
+                                    subMatrix[i, j] = originalMatrix[i, j];
+                                }
+                            }
+                        }
+                        break;
+
+                    case "Русские и латинские символы":
+                        for (int i = 0; i < subMatrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < subMatrix.GetLength(1); j++)
+                            {
+                                if (Regex.IsMatch(subMatrix[i, j], @"[a-zA-Z]") & Regex.IsMatch(subMatrix[i, j], @"[\p{IsCyrillic}]"))
+                                {
+                                }
+                                else
+                                {
+                                    subMatrix[i, j] = originalMatrix[i, j];
+                                }
+                            }
+                        }
+                        break;
+
+                }
+
+                dataGridView1.ColumnCount = userColumnCount;
+                dataGridView1.RowCount = userRowCount;
+                for (int i = 0; i < userRowCount; i++)
+                {
+                    for (int j = 0; j < userColumnCount; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = subMatrix[i, j];
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка\nПроверьте введенную размерность матрицы");
+            }
+        }
+
+    }
+}
